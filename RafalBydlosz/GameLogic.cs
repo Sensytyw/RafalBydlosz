d69738bf1cd5d5d81cf3721b7185a2f6440b4654
Rafał Bydłosz <rafalbydlosz@gmail.com>
2022-11-09 16:34:49 +0100

    Prawie gotowe, zostało tylko zaimplementować zablokowanie możliwości klikania po wygranej

--- RafalBydlosz/GameLogic.cs
+++ RafalBydlosz/GameLogic.cs
@@ -11,7 +11,7 @@ namespace RafalBydlosz
 		public string CurrentPlayer { get; set; } = X;
 		private const string X = "X";
 		private const string O = "O";
-
+		private string[,] Board = new string[3, 3];
 
 		public void SetNextPlayer()
 		{
@@ -25,6 +25,52 @@ namespace RafalBydlosz
 			{
 				CurrentPlayer = X;
 			}
+		}
+		public bool PlayerWin()
+		{
+			for(var i=0; i < 3; i++)
+			{
+				if(!String.IsNullOrWhiteSpace(Board[i,0]))
+				{
+					if (Board[i,0] == Board[i,1] && Board[i,0]== Board[i,2])
+					{
+						return true;
+					}
+				}
+			}
+
+			for (var i = 0; i < 3; i++)
+			{
+				if (!String.IsNullOrWhiteSpace(Board[0,i]))
+				{
+					if (Board[0,i] == Board[0,i] && Board[0,i] == Board[2,i])
+					{
+						return true;
+					}
+				}
+			}
+
+			
+				if (!String.IsNullOrWhiteSpace(Board[1, 1]))
+				{
+					if (Board[0, 0] == Board[1, 1] && Board[1, 1] == Board[2, 2])
+					{
+						return true;
+					}
+				if (Board[0, 2] == Board[1, 1] && Board[1, 1] == Board[2, 0])
+				{
+					return true;
+				}
+			}
+
+
+
+
+			return false;
+		}
+		internal void UpdateBoard(Position position, string value)
+		{
+			Board[position.x, position.y] = value;
 		}
 
 	}
--- RafalBydlosz/TicTacToe.xaml.cs
+++ RafalBydlosz/TicTacToe.xaml.cs
@@ -32,11 +32,35 @@ namespace RafalBydlosz
 			if (!String.IsNullOrWhiteSpace(space.Content?.ToString())) return;
 			space.Content = _GameLogic.CurrentPlayer;
 
+			var coordinates = space.Tag.ToString().Split(',');
+			var xValue = int.Parse(coordinates[0]);
+			var yValue = int.Parse(coordinates[1]);
+
+			var buttonPosition = new Position()
+			{ x=xValue, y=yValue};
+
+			_GameLogic.UpdateBoard(buttonPosition, _GameLogic.CurrentPlayer);
+			if(_GameLogic.PlayerWin())
+			{
+				WinScreen.Text = $"{_GameLogic.CurrentPlayer} WINS :)";
+				WinScreen.Visibility = Visibility.Visible;
+			}
+
+
 			_GameLogic.SetNextPlayer();
 		}
 
 		private void NewGame_Click(object sender, RoutedEventArgs e)
 		{
+			foreach( var control in gridBoard.Children)
+			{
+				if(control is Button)
+				{
+					((Button)control).Content = String.Empty;
+				}
+			}
+			_GameLogic = new GameLogic();
+			WinScreen.Visibility = Visibility.Collapsed;
 
 		}
 	}
