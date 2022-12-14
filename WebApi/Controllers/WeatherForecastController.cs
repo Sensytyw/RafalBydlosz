8ebd06644e24f2d6d94355c278c5f5ea25dfb894
Rafał Bydłosz <rafalbydlosz@gmail.com>
2022-12-14 16:37:17 +0100

    Zajecia

--- WebApi/Controllers/WeatherForecastController.cs
+++ WebApi/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Service.Interfaces;
 
 namespace WebApi.Controllers
 {
@@ -6,6 +7,7 @@ namespace WebApi.Controllers
 	[Route("[controller]")]
 	public class WeatherForecastController : ControllerBase
 	{
+		private readonly ITest _test;
 		private static readonly string[] Summaries = new[]
 		{
 		"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -13,14 +15,16 @@ namespace WebApi.Controllers
 
 		private readonly ILogger<WeatherForecastController> _logger;
 
-		public WeatherForecastController(ILogger<WeatherForecastController> logger)
+		public WeatherForecastController(ITest test ,ILogger<WeatherForecastController> logger)
 		{
+			_test = test;
 			_logger = logger;
 		}
 
 		[HttpGet(Name = "GetWeatherForecast")]
 		public IEnumerable<WeatherForecast> Get()
 		{
+			_test.metodaTestowa();
 			return Enumerable.Range(1, 5).Select(index => new WeatherForecast
 			{
 				Date = DateTime.Now.AddDays(index),
